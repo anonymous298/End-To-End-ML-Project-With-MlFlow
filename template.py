e8ac24b0523be569e608cea25a74829a0ae7b658
anonymous298 <tackletalha@gmail.com>
2024-12-22 16:55:04 +0500

    changes

--- template.py
+++ template.py
@@ -4,20 +4,19 @@ from pathlib import Path
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = 'End-To-End-Project'
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/utils/common.py",
-    f"src/{project_name}/config/__init__.py",
-    f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/__init__.py",
-    f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/entity/config_entity.py",
-    f"src/{project_name}/constants/__init__.py",
+    f"src/__init__.py",
+    f"src/components/__init__.py",
+    f"src/utils/__init__.py",
+    f"src/utils/common.py",
+    f"src/config/__init__.py",
+    f"src/config/configuration.py",
+    f"src/pipeline/__init__.py",
+    f"src/entity/__init__.py",
+    f"src/entity/config_entity.py",
+    f"src/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "schema.yaml",
